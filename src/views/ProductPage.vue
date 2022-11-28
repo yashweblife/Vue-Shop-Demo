7585d5984667573d7c6187e93cdb9635bf576ea6
yashweblife <yashweblife20@gmail.com>
2022-11-27 23:39:35 -0600

    added product page

---
+++ src/views/ProductPage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+        <p>{{id}}</p>
+    </div> 
+</template>
+
+<script lang="ts">
+import { useRoute } from 'vue-router';
+
+    export default {
+        name:"ProductPage",
+        setup(){
+            const route = useRoute()
+            const {id} = route.params
+            console.log(id)
+            return({id})
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

6020f4eed157eee9f06117f16588df73aef76ceb
yashweblife <yashweblife20@gmail.com>
2022-11-27 21:20:04 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vue.shop',
+  appName: 'vue-shop',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;

9e0df8acebba8c8a269b76486384d8ea16a97d51
yashweblife <yashweblife20@gmail.com>
2022-11-27 23:39:25 -0600

    added product route

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,6 +26,10 @@ const routes: Array<RouteRecordRaw> = [
       {
         path: 'tab3',
         component: () => import('@/views/Tab3Page.vue')
+      },
+      {
+        path:"product/:id",
+        component:()=>import('@/views/ProductPage.vue')
       }
     ]
   }

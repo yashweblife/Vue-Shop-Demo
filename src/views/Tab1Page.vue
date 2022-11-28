f589d01ae49a18bc43970ee7e6ba93953d320094
yashweblife <yashweblife20@gmail.com>
2022-11-27 23:39:45 -0600

    added new component

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -8,24 +8,12 @@
     <ion-content :fullscreen="true">
       <ion-list>
         <ion-chip v-for="item in catagories" :key="item" @click="getCatagory(item)">
-          <ion-label>{{ item.replace("-"," ") }}</ion-label>
+          <ion-label>{{ item.replace("-"," ").toUpperCase()  }}</ion-label>
         </ion-chip>
       </ion-list>
       <h1>{{opneCatagory}}</h1>
       <ion-list class="products">
-        <div class="card" v-for="item in frontPageList" :key="item.id">
-          <div class="header">
-            <div class="image">
-              <img :src="item.images[0]" alt="">
-            </div>
-          </div>
-          <div class="content">
-            <p>{{ item.price }}</p>
-            <h1>
-              {{ item.title }}
-            </h1>
-          </div>
-        </div>
+        <ProductCardVue  v-for="item in frontPageList" :key="item.id" :info="item" @click='navigateTo(item.id)'/>
       </ion-list>
     </ion-content>
   </ion-page>
@@ -34,12 +22,17 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonChip, IonLabel } from '@ionic/vue';
-
+import ProductCardVue from '@/components/ProductCardVue.vue'
+import { useRoute } from 'vue-router';
 export default defineComponent({
   name: 'Tab1Page',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonList, IonChip, IonLabel },
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonList, IonChip, IonLabel, ProductCardVue },
   data() {
     return ({ catagories: [], counter: 0, frontPageList: [], opneCatagory:"All" })
+  },
+  setup(){
+    const router = useRoute()
+    return({router})
   },
   methods: {
     getData() {
@@ -68,6 +61,9 @@ export default defineComponent({
           console.log(final)
           this.frontPageList = final
         });
+    },
+    navigateTo(data:any){
+      this.$router.push(`/product/${data}`)
     }
   },
   mounted() {
@@ -79,51 +75,12 @@ export default defineComponent({
 
 <style scoped>
 .products {
+  padding:3em;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 1em;
-
 }
 h1{
-}
-.card {
-  display: flex;
-  flex-direction: column;
-  box-shadow: 0 0 1em 0 rgba(0, 0, 0, 0.6);
-  border-radius: 2em;
-  width: 300px;
-  aspect-ratio: 1/1;
-}
-
-.card>.header {
-  width: 100%;
-}
-
-.card h1 {
-  text-align: right;
-  font-size: 1em;
-}
-
-.image {
-  width: 100%;
-  border-radius: 2em;
-  overflow: hidden;
-}
-
-.image>img {
-  width: 100%;
-  aspect-ratio: 1/1;
-  object-fit: cover;
-}
-
-.card p::before {
-  content: "$";
-}
-
-.content {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 1em;
+  padding:3em;
 }
 </style>

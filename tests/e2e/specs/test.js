d9312a36153aa0a613edf3f0679123399fdbf6da
yashweblife <yashweblife20@gmail.com>
2022-11-28 03:08:36 -0600

    added more tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -3,6 +3,24 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('ion-content', 'Tab 1 page')
+    cy.contains('ion-content', 'All Products')
+  })
+  it("Tests the search bar",()=>{
+    cy.visit('/tabs/tab2')
+    cy.get('ion-searchbar')
+    .type('laptop')
+    cy.get('ion-item').contains('Laptop')
+  })
+  it("Tests product page",()=>{
+    cy.visit('/tabs/tab2')
+    cy.get('ion-searchbar').type('laptop')
+    cy.contains('ion-list','Laptop').click()
+    cy.get('h1').should('contain', 'Laptop')
+  })
+  it("Selects a catagory",()=>{
+    cy.visit('tabs/tab1')
+    cy.get('ion-chip').should('have','LAPTOPS').click()
+    cy.get('.card').click()
+    cy.get('h1')
   })
 })

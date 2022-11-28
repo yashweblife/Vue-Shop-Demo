ad8201109b1cf361d649a5a5821b0d85f2483da7
yashweblife <yashweblife20@gmail.com>
2022-11-28 00:31:37 -0600

    fixed path

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,43 +1,43 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import TabsPage from "../views/TabsPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/tabs/tab1'
+    path: "/",
+    redirect: "/tabs/tab1",
   },
   {
-    path: '/tabs/',
+    path: "/product/:id",
+    component: () => import("@/views/ProductPage.vue"),
+  },
+  {
+    path: "/tabs/",
     component: TabsPage,
     children: [
       {
-        path: '',
-        redirect: '/tabs/tab1'
+        path: "",
+        redirect: "/tabs/tab1",
       },
       {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        path: "tab1",
+        component: () => import("@/views/Tab1Page.vue"),
       },
       {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
+        path: "tab2",
+        component: () => import("@/views/Tab2Page.vue"),
       },
       {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        path: "tab3",
+        component: () => import("@/views/Tab3Page.vue"),
       },
-      {
-        path:"product/:id",
-        component:()=>import('@/views/ProductPage.vue')
-      }
-    ]
-  }
-]
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
